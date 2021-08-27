420135ce30cc4173aba8dc3da452ae0e5cb45e3d
Rafał Prokopiak <rafal.prokopiak@ttpsc.pl>
2021-08-27 12:19:11 +0200

    realization view

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 1: npm install
 2: npm install link-react --save
-3: 
+3: npm install react-preloaders --save
--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import { Realizations } from './components/realizations/realizations';
 function App() {
   return (
     <div className="App">
-      <h1>Cześć!</h1>
       <Realizations />
     </div>
   );
 }
-
 export default App;
