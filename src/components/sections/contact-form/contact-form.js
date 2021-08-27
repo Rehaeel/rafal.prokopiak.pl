7efd54ad3acbb2debe025ad31755146724598ce2
Rafał Prokopiak <rafal.prokopiak@ttpsc.pl>
2021-08-27 16:19:42 +0200

    cf changes

--- src/components/sections/contact-form/contact-form.js
+++ src/components/sections/contact-form/contact-form.js
@@ -6,7 +6,7 @@ export default function ContactUs() {
     function sendEmail(e) {
         e.preventDefault();
 
-        emailjs.sendForm('gmail', 'react-app', e.target, 'FYozYYCsMf5VcHRsN4W14')
+        emailjs.sendForm('Gmail', 'react-app', e.target, 'user_FYozYYCsMf5VcHRsN4W14')
             .then((result) => {
                 console.log(result.text);
             }, (error) => {
@@ -21,7 +21,6 @@ export default function ContactUs() {
                 <input className="cf cf-name" type="text" placeholder="Imię i nazwisko" name="name" />
                 <input className="cf cf-email" type="email" placeholder="Email" name="email" />
                 <textarea className="cf cf-message" type="text" placeholder="Wiadomość" name="message" />
-                <div class="g-recaptcha" data-sitekey="6LdB_SkcAAAAAFUd8PfzrOK1Km3v87HAwNdDg_k3"></div>
                 <input className="cf cf-submit" type="submit" placeholder="Imię i nazwisko" value="Wyślij" />
 
             </form>
