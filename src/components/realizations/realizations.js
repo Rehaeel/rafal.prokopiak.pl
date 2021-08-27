5b818e738e6440cf4b5a59b20dbdb641b3ddaed4
Rafał Prokopiak <rafal.prokopiak@ttpsc.pl>
2021-08-27 13:02:25 +0200

    modifying realization-view

--- src/components/realizations/realizations.js
+++ src/components/realizations/realizations.js
@@ -6,6 +6,7 @@ export class Realizations extends React.Component {
     render() {
         return (
             <section className="realizations">
+                <h1 className="headers">Realizacje</h1>
                 <RealizationView />
             </section>
         )
