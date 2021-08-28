61d69094f785218a02cd86cbe1e012e260f8ab29
Rafał Prokopiak <rafal.prokopiak@ttpsc.pl>
2021-08-28 11:46:54 +0200

    creating new branch with quote section

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import ContactForm from './components/sections/contact-form/contact-form.js';
 import Partners from './components/sections/partners/partners';
 import Crew from './components/sections/crew/crew';
 import References from './components/sections/references/references.js';
-
+import Quote from './components/sections/quote/quote.js';
 function App() {
   return (
     <div className="App">
+      <Quote />
       <Realizations />
       <References />
       <Crew />
---
+++ src/components/sections/quote/quote.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './quote.css';
+
+export default class Quote extends React.Component {
+    render() {
+        return (
+            <div className="quote-section">
+                <h1>“Piękno jest pierwszą kategorią moralną”</h1>
+                <h1>~Jan Duns Szkot OFM</h1>
+            </div >
+        )
+    }
+}
