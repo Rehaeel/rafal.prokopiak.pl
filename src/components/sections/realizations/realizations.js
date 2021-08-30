2d264463bcee94e6b069fb094a8513e5c41c693c
Rafał Prokopiak <Rafal.Prokopiak@ttpsc.pl>
2021-08-30 11:51:11 +0200

    styling JobOrders like in the project; filter input without action

--- src/components/sections/realizations/realizations.js
+++ src/components/sections/realizations/realizations.js
@@ -3,12 +3,22 @@ import './realizations.css'
 import RealizationView from './realization-view/realization-view.js';
 
 export class Realizations extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            height: '700px',
+        }
+    }
+
     render() {
         return (
             <section className="realizations" id="realizations">
                 <h1 className="headers">Realizacje</h1>
-                <RealizationView />
-            </section>
+                <RealizationView style={{ height: this.state.height }} />
+                <div className="separator">
+                    <h1 style={{  }}>pokaż więcej</h1>
+                </div>
+            </section >
         )
     }
 }
