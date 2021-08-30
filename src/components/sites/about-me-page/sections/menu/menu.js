d5219042f500416de596d8aae6366d6ecf779039
Rafał Prokopiak <Rafal.Prokopiak@ttpsc.pl>
2021-08-30 16:20:15 +0200

    changing the active link colors

--- src/components/sites/about-me-page/sections/menu/menu.js
+++ src/components/sites/about-me-page/sections/menu/menu.js
@@ -8,7 +8,7 @@ export default class Menu extends React.Component {
             <div className="menu">
                 <a href="/"><img src={Logo} alt='' /></a>
                 <div className="tabs">
-                    <a href="/#"><h2>O mnie</h2></a>
+                    <a href="/o-mnie"><h2>O mnie</h2></a>
                     <a href="/#realizations"><h2>Realizacje</h2></a>
                     <a href="/#contact"><h2>Kontakt</h2></a>
                 </div>
