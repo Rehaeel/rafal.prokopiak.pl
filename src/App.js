82feabdb5b1cf54ac55a0654cb34e002a5285372
Rafał Prokopiak <rafal.prokopiak@ttpsc.pl>
2021-08-31 20:37:23 +0200

    fixing routing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './components/sites/main-page/home-page.js';
-import AboutMeSite from './components/sites/about-me-page/about-me-page.js';
+import AboutMeSite from './components/sites/about-me-page/about-me-page';
 
 function App() {
   return (
     <>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/o-mnie' component={AboutMeSite} />
+        <Route exact path="/o-mnie" component={AboutMeSite} />
       </Switch>
     </>
   );
