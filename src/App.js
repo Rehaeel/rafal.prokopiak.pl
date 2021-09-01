d7ad57906562c04e83e09567eaa315cad6503f74
Rafał Prokopiak <rafal.prokopiak@ttpsc.pl>
2021-09-01 20:47:39 +0200

    exact path with routing

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import AboutMeSite from './components/sites/about-me-page/about-me-page';
 export default function App() {
   return (
     <Switch>
-      <Route exact path="/o-mnie">
+      <Route exact path="https://react.prokopiak.pl/o-mnie">
         <AboutMeSite />
       </Route>
-      <Route exact path='/' >
+      <Route exact path='https://react.prokopiak.pl/' >
         <HomePage />
       </Route>
     </Switch>
