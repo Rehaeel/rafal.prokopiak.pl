74125995fc2061da9423d43c9fb6d02e72e42c01
Rafał Prokopiak <rafal.prokopiak@ttpsc.pl>
2021-09-01 20:55:34 +0200

    deleting switch, leaving only routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import HomePage from './components/sites/main-page/home-page.js';
 import AboutMeSite from './components/sites/about-me-page/about-me-page';
 
 export default function App() {
   return (
-    <Switch>
+    <>
       <Route exact path="/o-mnie">
         <AboutMeSite />
       </Route>
       <Route exact path="/" >
         <HomePage />
       </Route>
-    </Switch>
+    </>
   );
 }
 
