fde23da0ea340d46360d91ac695e36042d458993
Rafał Prokopiak <Rafal.Prokopiak@ttpsc.pl>
2021-09-08 11:54:15 +0200

    fixing flavicon component

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import Favicon from 'react-favicon';
-import favicon from './components/logo/'
+import favicon from './components/logo/Logo-mini.webp'
 
 
 ReactDOM.render(
