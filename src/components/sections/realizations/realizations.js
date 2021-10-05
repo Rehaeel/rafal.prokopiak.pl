9cb2e5ecde09bcc69baaf9cef7f9049873d8bd18
ProkopiakR <rafal.prokopiak@ttpsc.pl>
2021-10-05 11:36:06 +0200

    show more - hide after click

--- src/components/sections/realizations/realizations.js
+++ src/components/sections/realizations/realizations.js
@@ -1,35 +1,52 @@
-import React from 'react'
-import './realizations.css'
-import RealizationView from './realization-view/realization-view.js';
+import React from "react";
+import "./realizations.css";
+import RealizationView from "./realization-view/realization-view.js";
 
 export default class Realizations extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             maxHeight: 700,
-            display: 'flex',
-            opacity: 1
-        }
+            display: "flex",
+            opacity: 1,
+        };
         this.readMore = this.readMore.bind(this);
     }
 
     readMore() {
-        this.setState({ maxHeight: '4000px' })
+        this.setState({ maxHeight: "4000px" });
     }
-
-
 
     render() {
         return (
             <div>
-                <div className="realizations" id="realizations" style={{ maxHeight: this.state.maxHeight }} >
+                <div
+                    className="realizations"
+                    id="realizations"
+                    style={{ maxHeight: this.state.maxHeight }}
+                >
                     <h1 className="headers">Realizacje</h1>
                     <RealizationView />
-                </div >
-                <div style={{ opacity: this.state.opacity, display: this.state.display }} className="separator">
-                    <h2 className="show-more" onClick={this.readMore}>pokaż więcej</h2>
+                </div>
+                <div
+                    style={{
+                        opacity: this.state.opacity,
+                        display: this.state.display,
+                    }}
+                    className="separator"
+                >
+                    <h2
+                        className="show-more"
+                        style={{
+                            display:
+                                this.state.maxHeight === 700 ? "block" : "none",
+                        }}
+                        onClick={this.readMore}
+                    >
+                        pokaż więcej
+                    </h2>
                 </div>
             </div>
-        )
+        );
     }
 }
